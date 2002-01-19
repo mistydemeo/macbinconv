1bf82e5ba2d84472dd27a244a2447d12ffe95e49
boga <>
2002-01-19 21:54:58 +0000

    Added support for getting the FXInfo, and FInfo data

--- Sources/Core/FileUtils.cpp
+++ Sources/Core/FileUtils.cpp
@@ -4,11 +4,16 @@ std::string FileUtils::GetFileNameFromPath(const std::string &path)
 {
 	int loc;
 
-#if defined(macintosh)
+#if defined(macintosh) 
+    #if TARGET_RT_MACH0
+        loc = path.find_last_of("\\/",path.size ());
+        if (loc != 0)
+            loc++;
+    #else
 	loc = path.find_last_of(':',path.size());
 	if (loc != 0)
 		loc++;
-	
+    #endif
 #else
 	loc = path.find_last_of("\\/",path.size());
 	if (loc != 0)
--- Sources/Formats/NativeMacIO.cpp
+++ Sources/Formats/NativeMacIO.cpp
@@ -12,17 +12,64 @@
     #include <Aliases.h>
 #endif
 
+#if TARGET_API_MAC_CARBON
+template <class T> class AutoCFType {
+public:
+        AutoCFType (const T& p) : mP (p) {}
+        AutoCFType () : mP (NULL) {}
+        
+        AutoCFType(AutoCFType<T> &r) : mP (r.relase ()) {}
+        
+        ~AutoCFType () { if (mP != NULL) CFRelease (mP); }
+    
+    AutoCFType<T>& operator= (AutoCFType<T>& r) { reset( r.release ()); return *this; }
+    T&		operator= (const T& p) { reset (p); return mP;  }
+    T 		get () { return mP; }
+    T		release () { T result = mP; mP = NULL; return result; }
+    void 	reset (const T& p) { if ((p != mP) && (mP != NULL)) CFRelease (mP); mP = const_cast<T>(p);   }
 
+private:
+    T	mP;
+};
+#endif
 NativeMacFileInput::NativeMacFileInput (const std::string &path)
 {
+	mFileName = FileUtils::GetFileNameFromPath(path);
+
+#if TARGET_API_MAC_CARBON && USE_HFS_PLUS
+        AutoCFType<CFStringRef>	pathstr (NULL);
+        AutoCFType<CFURLRef>	url (NULL);
+#if TARGET_RT_MAC_MACHO
+        CFURLPathStyle	pathstyle = kCFURLPOSIXPathStyle;
+#else
+        CFURLPathStyle 	pathstyle = kCFURLHFSPathStyle;
+#endif
+        pathstr = CFStringCreateWithCString (
+                    kCFAllocatorDefault,
+                    path.c_str (),
+                    CFStringGetSystemEncoding ());
+        if (pathstr.get () == NULL)
+            throw std::bad_alloc ();
+            
+        url = CFURLCreateWithFileSystemPath (
+                    kCFAllocatorDefault,
+                    pathstr.get (),
+                    pathstyle,
+                    false
+                );
+        if (url.get () == NULL)
+            throw std::bad_alloc ();
+        
+        if (!CFURLGetFSRef (url.get (), &mFileRef)) 
+            throw CL::BasicException ("File not found at path!\n");
+        
+#else    
 	AliasHandle	alias;
 	Boolean		wasChanged;
 	Str255		nullString;
 	OSErr		result;
-	
-	mFileName = FileUtils::GetFileNameFromPath(path);
-
-	nullString[0] = 0;	/* null string to indicate no zone or server name */
+        
+        nullString[0] = 0;	/* null string to indicate no zone or server name */
 	result = NewAliasMinimalFromFullPath(path.length(), path.c_str(), nullString, nullString, &alias);
 
 	if (result != noErr) 
@@ -33,9 +80,10 @@ NativeMacFileInput::NativeMacFileInput (const std::string &path)
 	result = ResolveAlias(NULL, alias, &mSpec, &wasChanged);
 #endif
 	DisposeHandle ((Handle)alias);
-	
+
 	if (result != noErr) 
 		throw CL::BasicException ("Unable to resolve path!\n");	
+#endif	
 }
 NativeMacFileInput::~NativeMacFileInput ()
 {
@@ -103,10 +151,21 @@ MacForkInputStream *NativeMacFileInput::openRF ()
 	return new NativeMacForkInputStream (theRefNum);
 }
 bool NativeMacFileInput::getInfo (	FInfo&		finderInfo,
-								FXInfo&		extendedInfo)
+					FXInfo&		extendedInfo)
 {
-	DefaultFInfo (finderInfo,extendedInfo);
-	return true;
+    DefaultFInfo (finderInfo,extendedInfo);
+    
+    #if USE_HFS_PLUS
+        OSErr 		status;
+        FSCatalogInfo	info;
+    
+        status = FSGetCatalogInfo (&mFileRef, kFSCatInfoFinderInfo | kFSCatInfoFinderXInfo, &info, NULL, NULL, NULL);
+        if (status == noErr) {
+            finderInfo = *(reinterpret_cast<FInfo*>(info.finderInfo));
+            extendedInfo = *(reinterpret_cast<FXInfo*>(info.extFinderInfo));
+        }
+    #endif
+    return true;
 }
 bool  NativeMacFileInput::getComment ( std::string&	thecomment )
 {
